1c7fbf683c0654936f4bb04c953c69ef25016e79
jypark <jyp8637282@gmail.com>
2025-02-09 20:11:06 +0900

    🛠️fix: item dto 수정
    - 🛠️item dto에 university dto를 반환하도록 추가

--- src/main/java/com/hiccproject/moaram/dto/ItemDto.java
+++ src/main/java/com/hiccproject/moaram/dto/ItemDto.java
@@ -11,21 +11,39 @@ import lombok.Getter;
 public class ItemDto {
     private Long id;
     private String name;
+    private UniversityDto universityDto;
     private String location;
     private Integer price;
     private String description;
     private ItemStatus status;
-    private ExhibitionDto exhibition;  // ExhibitionDto 추가
+    private ExhibitionDto exhibitionDto;  // ExhibitionDto 추가
 
     public static ItemDto fromEntity(Item item, Exhibition exhibition) {
+        // exhibition 처리
         ExhibitionDto exhibitionDto = null;
         if (exhibition != null) {
             exhibitionDto = ExhibitionDto.fromEntity(exhibition);  // exhibition이 null이 아니면 fromEntity 실행
         }
 
+        // university 처리
+        UniversityDto universityDto = null;
+        if (item.getUniversity() != null) {
+            universityDto = new UniversityDto(
+                    item.getUniversity().getId(),
+                    item.getUniversity().getName(),
+                    item.getUniversity().getCampus(),
+                    item.getUniversity().getAddress(),
+                    new RegionDto(
+                            item.getUniversity().getRegion().getId(),
+                            item.getUniversity().getRegion().getName()
+                    )
+            );
+        }
+
         return new ItemDto(
                 item.getId(),
                 item.getName(),
+                universityDto,
                 item.getLocation(),
                 item.getPrice(),
                 item.getDescription(),
